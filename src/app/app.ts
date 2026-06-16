05f1ad6cdd1bf13758092932d1f68d11cba6ae0f
qadir <aqadir1989@gmail.com>
2026-06-17 00:50:09 +0530

    implemented-tosignal-to-fetch-the-data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,16 @@ import { ApiService } from './services/api.service';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('api-demo-mohit');
-  users: any = [];
+  userService = inject(ApiService);
+  users = toSignal(this.userService.getUsers(), { initialValue: [] });
 
-  constructor(private userService: ApiService) {}
+  // constructor(private userService: ApiService) {}
 
-  ngOnInit() {
-    console.log('call api');
-    this.userService.getUsers().subscribe((data: any) => {
-      this.users = data;
-      console.log(this.users);
-    });
-  }
+  // ngOnInit() {
+  //   console.log('call api');
+  //   this.userService.getUsers().subscribe((data: any) => {
+  //     this.users = data;
+  //     console.log(this.users);
+  //   });
+  // }
 }
